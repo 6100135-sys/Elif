ce9041a307593fe84cd663b71b79a03935e221f2
6100135-sys <6100135@spsd.sk.ca>
2026-09-21 14:10:18 -0600

    day 1 of 1st asssinment

---
+++ sketch.js
@@ -0,0 +1,20 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+async function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(width/2,height/2,80);
+  rect(width/2 += 40,height/2,5,80);
+  rect(width/2 -= 40,height/2,5,80);
+  style.color = "green"
+  noStroke
+}
